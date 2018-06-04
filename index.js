461d372aa55d74f9bd516d10626254308c40f41c
Sean Clifford <sclifford6262@gmail.com>
2018-06-04 23:21:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function getRepositories(){
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
 }
+
+function showRepositories(){
+  const repos = JSON.parse(this.responseText)
+  const repoList = `<ul>${repos.map(repo => '<li>' + repo.name + '</li>').join('')}`
+}
